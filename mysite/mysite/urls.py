7b81d455634892a48d71de1d573127b5f53785e7
Aleksan.Jhangiryan <lernagorc90@gmail.com>
2025-07-01 11:44:12 +0400

    Similar posts based on tags

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("blog.urls", namespace="blog")),
     path("admin/", admin.site.urls),
     path("blog/", include("blog.urls", namespace="blog")),
 ]
